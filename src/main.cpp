84e1fef73f76617cf36d4c0d1b183199e0b18f83
Resolt <philip@neuronsinc.com>
2018-08-16 17:09:29 +0200

    -added make and runs scripts

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
+#include <SDL2/SDL.h>
+
 int main()
 {
-	std::cout << "Greetings morons!" << std::endl;
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "SDL INIT FAILED!" << std::endl;
+		return 1;
+	}
+	else
+	{
+		std::cout << "Greetings morons!" << std::endl;
+	}
 
 	return 0;
 }
